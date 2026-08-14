df7819dd37386021394446884c05c95c96970c54
Lxk_buaa <867768510@qq.com>
2026-08-14 14:06:08 +1100

    ci: require the declared privacy scanner

--- .xgc2/scripts/check_client_privacy.sh
+++ .xgc2/scripts/check_client_privacy.sh
@@ -13,12 +13,7 @@ scan_text() {
   local target="$1"
   local status
   set +e
-  if command -v rg >/dev/null 2>&1; then
-    LC_ALL=C rg -a -q -i -- "${forbidden_regex}" "${target}"
-  else
-    # GitHub-hosted runners do not ship ripgrep; GNU grep is enough here.
-    LC_ALL=C grep -RaEq -i -- "${forbidden_regex}" "${target}"
-  fi
+  LC_ALL=C rg -a -q -i -- "${forbidden_regex}" "${target}"
   status=$?
   set -e
   case "${status}" in
